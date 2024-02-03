a3555f6d0934b884c63d3ecc3857345415468cbe
Wandile Gwebu <wandilegwebu8@gmail.com>
2024-02-03 15:41:03 +0200

    Add project files.

---
+++ MyBudget/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyBudget
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ MyBudget/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBudget
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
